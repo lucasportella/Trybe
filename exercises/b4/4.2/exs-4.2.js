97e227ba5c2a798d43c985ea6481b954056f1941
lucasportella <lucasportellaagu@gmail.com>
2021-02-25 13:41:07 -0300

    ex5

--- exercises/b4/4.2/exs-4.2.js
+++ exercises/b4/4.2/exs-4.2.js
@@ -1,12 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
+let maiorValor = 0;
 for (contador = 0; contador < numbers.length; contador++) {
-    soma += numbers[contador];
+    if (numbers[contador] > maiorValor) {
+        maiorValor = numbers[contador]
+    }
 }
-console.log(soma/numbers.length);
-
-if (soma/numbers.length > 20) {
-    console.log('valor maior que 20')
-} else {
-    console.log('valor menors que 20')
-}
+console.log(maiorValor);
