27c748e20fe1e7242a61aaca2616984eb1e696a5
lucasportella <lucasportellaagu@gmail.com>
2021-02-25 13:47:46 -0300

    ex7

--- exercises/b4/4.2/exs-4.2.js
+++ exercises/b4/4.2/exs-4.2.js
@@ -1,8 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let oddCounter = 0;
+let lowestNumber = 999999999;
 for (contador = 0; contador < numbers.length; contador++) {
-    if (numbers[contador] % 2 != 0) {
-        oddCounter++;
+    if (numbers[contador] < lowestNumber) {
+        lowestNumber = numbers[contador];
     }
 }
-console.log(oddCounter);
+console.log(lowestNumber);
