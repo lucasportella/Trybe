aee584a02ea302cd918688970898683906448e69
lucasportella <lucasportellaagu@gmail.com>
2021-02-25 17:01:30 -0300

    ex4.2 bonus decrescent order done

--- exercises/b4/4.2/ex-4.2-bonus.js
+++ exercises/b4/4.2/ex-4.2-bonus.js
@@ -1,7 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let c1 = 1; c1 < numbers.length; c1++) {
     for (let c2 = 0; c2 < c1; c2++) {
-        if (numbers[c1] < numbers[c2]) {
+        if (numbers[c1] > numbers[c2]) {
             let position = numbers[c1];
                
             numbers[c1] = numbers[c2];
