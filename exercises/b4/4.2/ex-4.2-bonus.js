22d43662bde6d12f3047dbd485333920cd79889b
lucasportella <lucasportellaagu@gmail.com>
2021-02-25 16:40:36 -0300

    ex4.2 bonus crescent order done

--- exercises/b4/4.2/ex-4.2-bonus.js
+++ exercises/b4/4.2/ex-4.2-bonus.js
@@ -1,11 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
 for (let c1 = 1; c1 < numbers.length; c1++) {
     for (let c2 = 0; c2 < c1; c2++) {
-        if (numbers[c2] > numbers[c1]) {
-            let position = c1;
-                c1 = c2;
-                c2 = position;
+        if (numbers[c1] < numbers[c2]) {
+            let position = numbers[c1];
+               
+            numbers[c1] = numbers[c2];
+            numbers[c2] = position;
         }
     }
 }
+console.log(numbers)
