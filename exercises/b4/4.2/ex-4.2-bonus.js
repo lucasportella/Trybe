f1d26cdffbccbda6cca9abfe541d92b151c7aeeb
lucasportella <lucasportellaagu@gmail.com>
2021-02-25 20:54:30 -0300

    bonus ex3 almost done-missing last array element

--- exercises/b4/4.2/ex-4.2-bonus.js
+++ exercises/b4/4.2/ex-4.2-bonus.js
@@ -1,12 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (let c1 = 1; c1 < numbers.length; c1++) {
-    for (let c2 = 0; c2 < c1; c2++) {
-        if (numbers[c1] > numbers[c2]) {
-            let position = numbers[c1];
-               
-            numbers[c1] = numbers[c2];
-            numbers[c2] = position;
-        }
+for (let c1 = 0; c1 < (numbers.length - 1); c1+=0) {
+    if (c1 == (numbers.length - 1)) {
+        numbers[length] = numbers[length] * 2
+        c1 = 10
+        c2 = 10
+    }
+    for (let c2 = 1; c2 > c1; c2 += 1 ) {
+        numbers[c1] = numbers[c1] * numbers[c2];
+        c1 += 1
+        console.log(numbers) 
     }
 }
-console.log(numbers)
+console.log(numbers) 
