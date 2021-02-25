e452ca8f5005aff3dedbcbf3938cb5012d241dcf
lucasportella <lucasportellaagu@gmail.com>
2021-02-25 13:43:18 -0300

    ex6

--- exercises/b4/4.2/exs-4.2.js
+++ exercises/b4/4.2/exs-4.2.js
@@ -1,9 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
-let maiorValor = 0;
+let oddCounter = 0;
 for (contador = 0; contador < numbers.length; contador++) {
-    if (numbers[contador] > maiorValor) {
-        maiorValor = numbers[contador]
+    if (numbers[contador] % 2 != 0) {
+        oddCounter++;
     }
 }
-console.log(maiorValor);
+console.log(oddCounter);
