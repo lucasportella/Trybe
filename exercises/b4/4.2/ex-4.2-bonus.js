28f551ca8fc6cefa33d6241a5e704a751baf2636
lucasportella <lucasportellaagu@gmail.com>
2021-02-25 21:23:35 -0300

    bonus ex3 adjusts

--- exercises/b4/4.2/ex-4.2-bonus.js
+++ exercises/b4/4.2/ex-4.2-bonus.js
@@ -3,12 +3,11 @@ for (let c1 = 0; c1 < (numbers.length - 1); c1+=0) {
     for (let c2 = 1; c2 > c1; c2 += 1 ) {
         if (c1 >= (numbers.length - 1)) {
             numbers[numbers.length - 1] = numbers[numbers.length - 1] * 2
-            c1 = 50
-            c2 = 20
+            c1 = 9
+            c2 = 0
         } else {
         numbers[c1] = numbers[c1] * numbers[c2];
         c1 += 1
-        console.log(numbers)
         }
     }
 }
