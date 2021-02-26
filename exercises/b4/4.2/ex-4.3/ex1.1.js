039c42d96854bc49e26b9c297d9f2886fbd1e80c
lucasportella <lucasportellaagu@gmail.com>
2021-02-26 15:11:22 -0300

    ex1 another way + ex2

---
+++ exercises/b4/4.2/ex-4.3/ex1.1.js
@@ -0,0 +1,10 @@
+n = 5;
+let inputLine = '';
+let asterisk = '*'
+
+for (counter = 0; counter < n; counter += 1) {
+    inputLine = inputLine + asterisk;
+}
+for (counter = 0; counter < n; counter += 1) {
+    console.log(inputLine)
+}
---
+++ exercises/b4/4.2/ex-4.3/ex2.js
@@ -0,0 +1,7 @@
+n = 5;
+asterisk = '*';
+inputLine = '';
+for (counter = 0; counter < 5; counter += 1) {
+    inputLine = inputLine + asterisk
+    console.log(inputLine)
+}
