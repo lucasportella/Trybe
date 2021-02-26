eee1516a90a5eba177c6c65292a808d5c1bb0648
lucasportella <lucasportellaagu@gmail.com>
2021-02-25 21:18:21 -0300

    bonus ex3 working

--- exercises/b4/4.2/ex-4.2-bonus.js
+++ exercises/b4/4.2/ex-4.2-bonus.js
@@ -2,7 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let c1 = 0; c1 < (numbers.length - 1); c1+=0) {
     for (let c2 = 1; c2 > c1; c2 += 1 ) {
         if (c1 >= (numbers.length - 1)) {
-            numbers[numbers.length] = numbers[numbers.length] * 2
+            numbers[numbers.length - 1] = numbers[numbers.length - 1] * 2
             c1 = 50
             c2 = 20
         } else {
