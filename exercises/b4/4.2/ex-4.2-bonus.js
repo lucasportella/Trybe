ce6d2d8fa6cd62e8216b7592f6c54010e4347121
lucasportella <lucasportellaagu@gmail.com>
2021-02-25 21:12:29 -0300

    bonus ex3 very very close

--- exercises/b4/4.2/ex-4.2-bonus.js
+++ exercises/b4/4.2/ex-4.2-bonus.js
@@ -1,14 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let c1 = 0; c1 < (numbers.length - 1); c1+=0) {
-    if (c1 == (numbers.length - 1)) {
-        numbers[length] = numbers[length] * 2
-        c1 = 10
-        c2 = 10
-    }
     for (let c2 = 1; c2 > c1; c2 += 1 ) {
+        if (c1 >= (numbers.length - 1)) {
+            numbers[numbers.length] = numbers[numbers.length] * 2
+            c1 = 50
+            c2 = 20
+        } else {
         numbers[c1] = numbers[c1] * numbers[c2];
         c1 += 1
-        console.log(numbers) 
+        console.log(numbers)
+        }
     }
 }
 console.log(numbers) 
