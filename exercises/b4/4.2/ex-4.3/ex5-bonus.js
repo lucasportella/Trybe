a576ba96e27ff362b0ff400b5df18a2669e82c1d
lucasportella <lucasportellaagu@gmail.com>
2021-02-28 10:23:28 -0300

    ex5-bonus final adjust

--- exercises/b4/4.2/ex-4.3/ex5-bonus.js
+++ exercises/b4/4.2/ex-4.3/ex5-bonus.js
@@ -4,8 +4,8 @@ let leftInterval = middle;
 let rightInterval = middle;
 let printer = '';
 for (row = 1; row <= middle; row += 1) {
-  for (column = 1; column <= n - 1; column += 1) {
-    if (column == leftInterval || column == rightInterval ) {
+  for (column = 1; column <= n; column += 1) {
+    if (column == leftInterval || column == rightInterval || row == middle) {
       printer += '*';
     } else {
       printer += ' ';
@@ -16,10 +16,4 @@ for (row = 1; row <= middle; row += 1) {
   printer = '';
   leftInterval -= 1;
   rightInterval += 1;
-  if (leftInterval == 1) {
-      for (row = 1; row <= n; row += 1) {
-          printer += '*'
-      }
-    console.log(printer)
-  }
 }
