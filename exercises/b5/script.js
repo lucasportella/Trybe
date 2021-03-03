00dd25ea109f22c48a3baed18073bdb04139645d
lucasportella <lucasportellaagu@gmail.com>
2021-03-03 20:00:08 -0300

    practice 5.1

---
+++ exercises/b5/script.js
@@ -0,0 +1,17 @@
+let headerContainer = document.getElementById("header-container");
+headerContainer.style.backgroundColor = "green";
+
+let emergencyTasks = document.getElementsByClassName("emergency-tasks")[0];
+let noEmergencyTasks = document.getElementsByClassName("no-emergency-tasks")[0];
+emergencyTasks.style.backgroundColor = "salmon";
+noEmergencyTasks.style.backgroundColor = "yellow";
+
+let h3EmergencyTasks = document.querySelectorAll(".h3-1")[0];
+let h3NoemergencyTasks = document.querySelectorAll(".h3-2")[0];
+h3EmergencyTasks.style.backgroundColor = "purple";
+h3NoemergencyTasks.style.backgroundColor = "black";
+
+let h3EmergencyTasks2 = document.querySelectorAll(".h3-1")[1];
+let h3NoemergencyTasks2 = document.querySelectorAll(".h3-2")[1];
+h3EmergencyTasks2.style.backgroundColor = "purple";
+h3NoemergencyTasks2.style.backgroundColor = "black";
