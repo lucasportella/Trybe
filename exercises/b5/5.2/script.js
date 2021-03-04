95b2e0efa7b5b745539e75e6f0a4a33e4a9871a1
lucasportella <lucasportellaagu@gmail.com>
2021-03-04 13:44:55 -0300

    reorganize and 5.2 practice files

---
+++ exercises/b5/5.2/script.js
@@ -0,0 +1,15 @@
+let getAll = document.querySelectorAll("div");
+console.log(getAll)
+for (i = 0; i < getAll.length; i += 1) {
+    let elemento = document.querySelectorAll("div")[i];
+    console.log(i)
+    console.log(getAll.length)
+    console.log(elemento)
+    elemento.remove();
+    console.log(document.querySelectorAll("div"))
+}
+
+const ul = document.querySelector( 'ul' );
+
+console.log( ul.firstElementChild );
+console.log( ul.firstChild );
