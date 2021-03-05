184ff8b6f22e9b305bf50cb18d8831a68d5fb7bf
lucasportella <lucasportellaagu@gmail.com>
2021-03-05 19:28:51 -0300

    .target secret revealed to dynamic function

--- exercises/b5/5.3/practice-5.3/script.js
+++ exercises/b5/5.3/practice-5.3/script.js
@@ -1,12 +1,16 @@
 let caixaTexto = window.document.getElementsByClassName('caixa-texto');
 let caixa1 = window.document.getElementsByClassName('caixa1')[0];
-let caixa2 = window.document.getElementsByClassName('caixa2')[1];
+let caixa2 = window.document.getElementsByClassName('caixa2')[0];
 let botao1 = window.document.getElementsByClassName('botao1')[0];
-let botao2 = window.document.getElementsByClassName('botao1')[1];
+let botao2 = window.document.getElementsByClassName('botao2')[0];
 
-function corVerde(param) {
-  console.log(param)
-  param.style.backgroundColor = "green";
+function corVerde(parametro) {
+  parametro.target.style.backgroundColor = "green";
+  console.log(parametro);
 }
 
 caixa1.addEventListener('click', corVerde);
+caixa2.addEventListener('click', corVerde);
+botao1.addEventListener('click', corVerde);
+botao2.addEventListener('click', corVerde);
+
