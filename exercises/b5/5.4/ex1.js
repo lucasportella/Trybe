50cdcb02e3684cf4174022378b93b91433fb12df
lucasportella <lucasportellaagu@gmail.com>
2021-03-08 20:11:45 -0300

    5.4 ex1 part1 fail

--- exercises/b5/5.4/ex1.js
+++ exercises/b5/5.4/ex1.js
@@ -1,22 +1,33 @@
-let customize = 5;
-let getBody = document.getElementsByTagName('body')[0];
+const customize = 5;
+const getBody = document.getElementsByTagName('body')[0];
 for (i = 0; i < customize; i += 1) {
-    let newButton = document.createElement('button');
-    newButton.id = `botao${i+1}`
-    newButton.innerText = 'texto'
-    getBody.appendChild(newButton);
+  const newButton = document.createElement('button');
+  newButton.id = `botao${i + 1}`;
+  newButton.innerText = 'texto';
+  getBody.appendChild(newButton);
 }
 
-let bgColor = document.getElementById('botao1');
-let txtColor = document.getElementById('botao2');
-let fontSize = document.getElementById('botao3');
-let lineHeight = document.getElementById('botao4');
-let fontFamily = document.getElementById('botao5');
+const bgColor = document.getElementById('botao1');
+bgColor.innerText = 'bg-color-red';
 
+const txtColor = document.getElementById('botao2');
+const fontSize = document.getElementById('botao3');
+const lineHeight = document.getElementById('botao4');
+const fontFamily = document.getElementById('botao5');
 
-bgColor.innerText = 'bg-color-red'
-bgColor.addEventListener('click',changeBgColor)
-function changeBgColor() {
-    getBody.style.backgroundColor = "red";
-    localStorage.setItem("backgroundColor", "red");
+function changeBgColor(color) {
+  bgColor.addEventListener('click', function() {
+    getBody.style.backgroundColor = color;
+   backStore = localStorage.setItem('backgroundColor', color);
+  });
 }
+
+changeBgColor('green');
+
+function initialize() {
+  let backStore = localStorage.getItem('backgroundColor');
+  if (backStore) {
+  changeBgColor(backStore);
+} }
+
+initialize()
