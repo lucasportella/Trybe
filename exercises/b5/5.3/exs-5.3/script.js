a32912dc7241c6eb053592795cd16a8a1755f73f
lucasportella <lucasportellaagu@gmail.com>
2021-03-08 14:56:50 -0300

    eslint modifications

--- exercises/b5/5.3/exs-5.3/script.js
+++ exercises/b5/5.3/exs-5.3/script.js
@@ -1,147 +1,133 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
-  
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
-    };
-  };
-  
-  function createDaysOfTheMonth() {
-  let daysUl = document.getElementById('days');
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  }
+}
+
+function createDaysOfTheMonth() {
+  const daysUl = document.getElementById('days');
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
   for (let i = 0; i < dezDaysList.length; i += 1) {
-    let specificDay = document.createElement('li');
+    const specificDay = document.createElement('li');
     specificDay.innerHTML = dezDaysList[i];
-    specificDay.className += "day ";
-    if (specificDay.innerHTML === "25" || specificDay.innerHTML === '24' || specificDay.innerHTML === '31') {
-      specificDay.className += "holiday ";
+    specificDay.className += 'day ';
+    if (specificDay.innerHTML === '25' || specificDay.innerHTML === '24' || specificDay.innerHTML === '31') {
+      specificDay.className += 'holiday ';
     }
     if (specificDay.innerHTML === '4' || specificDay.innerHTML === '11' || specificDay.innerHTML === '18' || specificDay.innerHTML === '25') {
-      specificDay.className += "friday ";
+      specificDay.className += 'friday ';
     }
     daysUl.appendChild(specificDay);
   }
 }
-createDaysOfTheMonth()
-
-
-
+createDaysOfTheMonth();
 
 function criarBotaoFeriado(param) {
-let botaoConteiner = document.getElementsByClassName('buttons-container')[0];
-let botaoFeriados = document.createElement('button');
-botaoFeriados.id = 'btn-holiday';
-botaoFeriados.innerText = param;
-botaoConteiner.appendChild(botaoFeriados);
+  const botaoConteiner = document.getElementsByClassName('buttons-container')[0];
+  const botaoFeriados = document.createElement('button');
+  botaoFeriados.id = 'btn-holiday';
+  botaoFeriados.innerText = param;
+  botaoConteiner.appendChild(botaoFeriados);
 }
 criarBotaoFeriado('Feriados');
 
-
-
-let botaoFeriado = document.getElementById('btn-holiday');
-botaoFeriado.addEventListener('click', FeriadoTrocaCor)
-let newColor = "purple";
-let standardColor = "rgb(238,238,238)";
+const botaoFeriado = document.getElementById('btn-holiday');
+botaoFeriado.addEventListener('click', FeriadoTrocaCor);
+const newColor = 'purple';
+const standardColor = 'rgb(238,238,238)';
 
 function FeriadoTrocaCor() {
   diasDeFeriado = document.getElementsByClassName('holiday');
-  console.log(diasDeFeriado)
-  for (let i in [diasDeFeriado]) {
-    let diaComFeriado = diasDeFeriado[i];
-  if (diaComFeriado.style.backgroundColor === newColor) {
-    diaComFeriado.style.backgroundColor = standardColor;
-  } else {
-    diaComFeriado.style.backgroundColor = newColor;
-  }
+  for (const i in [diasDeFeriado]) {
+    const diaComFeriado = diasDeFeriado[i];
+    if (diaComFeriado.style.backgroundColor === newColor) {
+      diaComFeriado.style.backgroundColor = standardColor;
+    } else {
+      diaComFeriado.style.backgroundColor = newColor;
+    }
   }
 }
 
 function sexta(param) {
-
-  let botaoSexta = document.createElement("button");
-  let containerButton = document.getElementsByClassName("buttons-container")[0];
-  containerButton.appendChild(botaoSexta)
-  botaoSexta.id = "btn-friday";
+  const botaoSexta = document.createElement('button');
+  const containerButton = document.getElementsByClassName('buttons-container')[0];
+  containerButton.appendChild(botaoSexta);
+  botaoSexta.id = 'btn-friday';
   botaoSexta.innerText = param;
 }
-sexta('Sexta-feira')
+sexta('Sexta-feira');
 
-
-
-let botaoSexta = document.getElementById('btn-friday');
+const botaoSexta = document.getElementById('btn-friday');
 botaoSexta.addEventListener('click', clickToSexta);
 let originColor = true;
-let dezFridays = [ 4, 11, 18, 25 ]
+const dezFridays = [4, 11, 18, 25];
 function clickToSexta() {
   diasDeSexta = document.getElementsByClassName('friday');
   for (i = 0; i < diasDeSexta.length; i += 1) {
     if (originColor === true) {
-      diasDeSexta[i].innerText = "SEXTOU!"
+      diasDeSexta[i].innerText = 'SEXTOU!';
     } else {
       diasDeSexta[i].innerText = dezFridays[i];
     }
   }
-    if (originColor === true) {
-      originColor = false;
-    } else {
-      originColor = true;
-    }
+  if (originColor === true) {
+    originColor = false;
+  } else {
+    originColor = true;
+  }
 }
 
-let days = document.getElementById('days');
+const days = document.getElementById('days');
 days.addEventListener('mouseover', zoom);
 days.addEventListener('mouseout', zoomOut);
 function zoom(event) {
-  event.target.style.fontSize = "30px";
+  event.target.style.fontSize = '30px';
 }
 
 function zoomOut(event) {
-  event.target.style.fontSize = "20px"
+  event.target.style.fontSize = '20px';
 }
 
-days.addEventListener('click', function(event){
-  let tarefaColor = document.getElementsByClassName('task')[0];
+days.addEventListener('click', (event) => {
+  const tarefaColor = document.getElementsByClassName('task')[0];
   event.target.style.backgroundColor = tarefaColor.style.backgroundColor;
-})
+});
 
-
-function newTask (task) {
-  let myTasks = document.getElementsByClassName("my-tasks")[0];
-  let cozinhar = document.createElement('span');
+function newTask(task) {
+  const myTasks = document.getElementsByClassName('my-tasks')[0];
+  const cozinhar = document.createElement('span');
   cozinhar.innerHTML = task;
   myTasks.appendChild(cozinhar);
 }
-newTask('Cozinhar')
+newTask('Cozinhar');
 
 function addSubtitles(color) {
-  let tarefa = document.getElementsByClassName('my-tasks')[0];
-  let novaTarefa = document.createElement('div');
-  novaTarefa.className = "task";
+  const tarefa = document.getElementsByClassName('my-tasks')[0];
+  const novaTarefa = document.createElement('div');
+  novaTarefa.className = 'task';
   tarefa.appendChild(novaTarefa);
-  novaTarefa.style.backgroundColor = color
+  novaTarefa.style.backgroundColor = color;
 }
 addSubtitles('purple');
 
-
-let tarefa = document.getElementsByClassName('task')[0];
-tarefa.addEventListener("click", clickTarefa)
+const tarefa = document.getElementsByClassName('task')[0];
+tarefa.addEventListener('click', clickTarefa);
 let verificador = true;
-
 
 function clickTarefa() {
   if (verificador === true) {
-  tarefa.className = "task selected";
-  verificador = false;
-} else {
-  tarefa.className = "task";
-  verificador = true;
+    tarefa.className = 'task selected';
+    verificador = false;
+  } else {
+    tarefa.className = 'task';
+    verificador = true;
+  }
 }
-}
-
-
