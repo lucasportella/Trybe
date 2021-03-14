08e5d8a9d021e75473a94c8d2ec7aaa80abf4102
lucasportella <lucasportellaagu@gmail.com>
2021-03-14 15:21:57 -0300

    ex beginning

---
+++ exercises/b6/6.1/script.js
@@ -0,0 +1,39 @@
+const statesContainer = document.querySelectorAll('select')[0];
+let states = [
+    'AC - Acre',
+    'AL - Alagoas',
+    'AP - Amapá',
+    'AM - Amazonas',
+    'BA - Bahia',
+    'CE - Ceará',
+    'DF - Distrito Federal',
+    'ES - Espírito Santo',
+    'GO - Goiás',
+    'MA - Maranhão',
+    'MT - Mato Grosso',
+    'MS - Mato Grosso do Sul',
+    'MG - Minas Gerais',
+    'PA - Pará',
+    'PB - Paraíba',
+    'PR - Paraná',
+    'PE - Pernambuco',
+    'PI - Piauí',
+    'RJ - Rio de Janeiro',
+    'RN - Rio Grande do Norte',
+    'RS - Rio Grande do Sul',
+    'RO - Rondônia',
+    'RR - Roraíma',
+    'SC - Santa Catarina',
+    'SP - São Paulo',
+    'SE - Sergipe',
+    'TO - Tocantins'
+    ];
+
+for (let i in states) {
+    let state = document.createElement('option');
+    console.log(state)
+    state.value = states[i];
+    state.innerText = states[i];
+    console.log(state.innerText);
+     statesContainer.appendChild(state);
+}
