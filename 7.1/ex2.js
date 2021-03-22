bc34610063f6699e2dced256516f22476ce05cf8
lucasportella <lucasportellaagu@gmail.com>
2021-03-22 10:56:55 -0300

    bubble sort splice

--- 7.1/ex2.js
+++ 7.1/ex2.js
@@ -1,12 +1,11 @@
 const oddsAndEvens = [13, 3, 4, 10, 2, 3, 25, 9, 7, 4, 7, 2];
 
-for (let i = 0; i < oddsAndEvens.length; i += 1) {
-    for (let j = 0; j <oddsAndEvens.length; j += 1) {
+for (let i = 0; i < oddsAndEvens.length - 1; i += 1) {
+    for (let j = 0; j <oddsAndEvens.length -1; j += 1) {
     if (oddsAndEvens[j] > oddsAndEvens[j + 1]) {
         let greater = oddsAndEvens[j];
-        let lesser = oddsAndEvens[j + 1]
-        oddsAndEvens[j] = lesser;
-        oddsAndEvens[j + 1] = greater
+        oddsAndEvens.splice(j, 1);
+        oddsAndEvens.splice(j + 1, 0, greater);
     }
 }
 }
