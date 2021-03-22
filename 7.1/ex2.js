cd8e010135b75a0622decfcae99fe54a47609cb7
lucasportella <lucasportellaagu@gmail.com>
2021-03-22 11:04:48 -0300

    ex2 bonus

--- 7.1/ex2.js
+++ 7.1/ex2.js
@@ -1,14 +1,6 @@
-const oddsAndEvens = [13, 3, 4, 10, 2, 3, 25, 9, 7, 4, 7, 2];
+let oddsAndEvens = [13, 3, 4, 10, 2, 3, 25, 9, 7, 4, 7, 2];
 
-for (let i = 0; i < oddsAndEvens.length - 1; i += 1) {
-    for (let j = 0; j <oddsAndEvens.length -1; j += 1) {
-    if (oddsAndEvens[j] > oddsAndEvens[j + 1]) {
-        let greater = oddsAndEvens[j];
-        oddsAndEvens.splice(j, 1);
-        oddsAndEvens.splice(j + 1, 0, greater);
-    }
-}
-}
+oddsAndEvens = oddsAndEvens.sort();
 
 let stringOddsAndEvens = "";
 for (let i = 0; i < oddsAndEvens.length; i += 1){
