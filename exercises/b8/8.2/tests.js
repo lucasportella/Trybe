32d7ce8828c6cda9947ed1f0da34b5e10c24469d
lucasportella <lucasportellaagu@gmail.com>
2021-03-30 15:55:22 -0300

    ex1

---
+++ exercises/b8/8.2/tests.js
@@ -0,0 +1,8 @@
+const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (name, index) => {
+  console.log(name[index]);
+};
+
+names.forEach(convertToUpperCase);
+// console.log(names);
