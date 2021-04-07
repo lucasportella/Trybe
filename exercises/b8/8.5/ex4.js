fa097d7b6d703edfc6b44215279e15620c2d1fed
lucasportella <lucasportellaagu@gmail.com>
2021-04-07 12:57:39 -0300

    ex4 withtout destructuring

---
+++ exercises/b8/8.5/ex4.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+const filterPeople = (param) => param.filter(person => person.bornIn >= 1900 && person.bornIn < 2000 && person.nationality === 'Australian');
+const filteredPeople = filterPeople(people);
+
+assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
+assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });
