2b1b3e0e62e3b0aca8faa00d5a909149995125e7
lucasportella <lucasportellaagu@gmail.com>
2021-04-14 09:06:37 -0300

    ex4?

--- exercises/b9/9.2/base.js
+++ exercises/b9/9.2/base.js
@@ -1,4 +1,5 @@
 // apiScript.js
+
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
@@ -8,17 +9,28 @@ const fetchJoke = () => {
   };
   fetch(API_URL, myObj)
   .then(response => response.json())
-  .then((joke) => console.log(joke.joke))
+  .then((joke) => document.getElementById('jokeContainer').innerText = joke.joke)
 };
 
 const myPromise = new Promise((resolve,reject) => {
-  const pickNumber = () => Math.floor(Math.random() * 51)
+ setTimeout(() => {
+  const pickNumber = () => (Math.floor(Math.random() * 50) + 1 ) ** 2
   const arrayOfNumbers = [];
   for (let i = 0; i < 10; i += 1) {
     arrayOfNumbers.push(pickNumber());
   }
-  console.log(arrayOfNumbers);
-});
+  const arraySum = arrayOfNumbers.reduce((acc, number) => acc + number);
+  if (arraySum < 8000) {
+   resolve(console.log('sucesso'))
+  } else {
+    reject(console.log('fracasso'))
+  }
+ },3000)
+})
+console.log(typeof(myPromise));
+
+myPromise.then(() => console.log('aaa'))
+console.log('bbb');
 
 
 window.onload = () => fetchJoke();
