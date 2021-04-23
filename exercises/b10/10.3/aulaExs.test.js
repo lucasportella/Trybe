7e509dfc4161e808a9c015fb3254496c504a9401
lucasportella <lucasportellaagu@gmail.com>
2021-04-22 21:54:14 -0300

    aulaExs part2 done refactored with jest.mock

--- exercises/b10/10.3/aulaExs.test.js
+++ exercises/b10/10.3/aulaExs.test.js
@@ -1,25 +1,26 @@
 const math = require("./math");
+jest.mock('./math');
 
 describe("mock math func", () => {
   test("should mock subtrair func", () => {
-    math.subtrair = jest.fn();
     math.subtrair();
+    math.subtrair.mockImplementation((a,b) => a - b);
     expect(math.subtrair).toHaveBeenCalled();
+    expect(math.subtrair(10,5)).toBe(5)
   });
   test('mock multiplicar return "10"', () => {
-    math.multiplicar = jest.fn().mockReturnValue("10");
+    math.multiplicar.mockReturnValue("10");
     expect(math.multiplicar()).toBe("10");
     expect(math.multiplicar).toHaveBeenCalled();
   });
   test("mock somar and implement sum return sum", () => {
-    math.somar = jest.fn().mockImplementation((a, b) => a + b);
+    math.somar.mockImplementation((a, b) => a + b);
     expect(math.somar(10, 8)).toBe(18);
     expect(math.somar).toHaveBeenCalledWith(10, 8);
     expect(math.somar).toHaveBeenCalled();
   });
   test("mock dividir return default 15, first 2, second 5", () => {
-    math.dividir = jest
-      .fn()
+    math.dividir
       .mockReturnValue(15)
       .mockReturnValueOnce(2)
       .mockReturnValueOnce(5);
@@ -34,8 +35,7 @@ describe("mock math func", () => {
     expect(math.dividir).toHaveBeenCalledWith();
   });
   test('mock subtrair restaure original, return default 20', () => {
-    math.subtrair = jest.fn()
-    math.subtrair.mockImplementation((a,b) => a -b)
+    math.subtrair.mockImplementation((a,b) => a - b)
     math.subtrair.mockImplementationOnce((a,b) => 20)
     expect(math.subtrair(5,3)).toBe(20);
     expect(math.subtrair).toHaveBeenCalled()
