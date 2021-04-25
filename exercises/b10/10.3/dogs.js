8dee936e6e464b9954c6e3e75c381e155527c5b8
lucasportella <lucasportellaagu@gmail.com>
2021-04-25 10:01:01 -0300

    create dog function

--- exercises/b10/10.3/dogs.js
+++ exercises/b10/10.3/dogs.js
@@ -1,20 +1,19 @@
+function createDog(dog) {
+  const doguinho = document.createElement("img");
+  doguinho.src = dog;
+  doguinho.width = 400;
+  const body = document.getElementsByTagName("body")[0];
+  body.appendChild(doguinho);
+}
+
 async function fetchDog() {
   const dog = await fetch("https://dog.ceo/api/breeds/image/random")
     .then((resp) => resp.json())
-    .then((json) => (json.message));
+    .then((json) => json.message);
 
-    const doguinho = document.createElement("img");
-    doguinho.src = dog;
-    const body = document.getElementsByTagName("body")[0];
-    body.appendChild(doguinho);
-    
-
+  createDog(dog)
 }
-fetchDog()
-fetchDog()
-fetchDog()
-fetchDog()
-fetchDog()
 
-
-
+for (let index = 0; index < 50; index += 1) {
+  fetchDog();
+}
