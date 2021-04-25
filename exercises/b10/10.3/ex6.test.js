2746f1aa3fb8ddde74a278eb784181518044eec7
lucasportella <lucasportellaagu@gmail.com>
2021-04-25 13:53:37 -0300

    ex6 done

--- exercises/b10/10.3/ex6.test.js
+++ exercises/b10/10.3/ex6.test.js
@@ -1,13 +1,25 @@
 const getDog = require('./ex6fetchFunc')
 
 describe('mock fetchDog function', () => {
-  const doggy = jest.spyOn(getDog, 'fetchDog')
-  it('should mock and test fetchDog', async () => {
-   doggy.mockResolvedValue('request success')
-   doggy();
-    expect(doggy).toHaveBeenCalled();
-    expect(doggy).toHaveBeenCalledTimes(1);
-    expect(doggy()).resolves.toEqual('request success')
+  getDog.fetchDog = jest.fn()
+
+afterEach(getDog.fetchDog.mockReset)
+
+  it('should mock and return resolved value as request success', async () => {
+   getDog.fetchDog.mockResolvedValue('request success')
+   getDog.fetchDog();
+    expect(getDog.fetchDog).toHaveBeenCalled();
+    expect(getDog.fetchDog).toHaveBeenCalledTimes(1);
+    expect(getDog.fetchDog()).resolves.toEqual('request success')
   })
+
+  it('should mock and return reject value as request failed', () => {
+     getDog.fetchDog.mockRejectedValue('request failed');
+     getDog.fetchDog();
+     expect(getDog.fetchDog).toHaveBeenCalled();
+     expect(getDog.fetchDog).toHaveBeenCalledTimes(1);
+     expect(getDog.fetchDog()).rejects.toEqual('request failed')
+  })
+
 })
 
