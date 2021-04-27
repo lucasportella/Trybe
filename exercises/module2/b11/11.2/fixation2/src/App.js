6951f6c12a9c5ff949adc48184b68abf49d1f9e7
lucasportella <lucasportellaagu@gmail.com>
2021-04-27 18:46:06 -0300

    fixation 2 dynamic props with map

--- exercises/module2/b11/11.2/fixation2/src/App.js
+++ exercises/module2/b11/11.2/fixation2/src/App.js
@@ -1,26 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import UserProfile from './UserProfile'
+import logo from "./logo.svg";
+import "./App.css";
+import UserProfile from "./UserProfile";
 
 function App() {
-  const joao = {
-    id: 102,
-    name: "João",
-    email: "joao@gmail.com",
-    avatar: "https:\/\/cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
-  };
-
-  const amelia = {
-    id: 77,
-    name: "Amélia",
-    email: "amelia@gmail.com",
-    avatar: "https:\/\/cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
-  };
+  const list = [
+    {
+      id: 102,
+      name: "João",
+      email: "joao@gmail.com",
+      avatar:
+        "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png",
+    },
+    {
+      id: 77,
+      name: "Amélia",
+      email: "amelia@gmail.com",
+      avatar:
+        "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png",
+    },
+  ];
 
   return (
     <div>
-    <UserProfile person={joao}></UserProfile>
-    <UserProfile person={amelia}></UserProfile>
+      {list.map((person) => (
+        <UserProfile person={person}></UserProfile>
+      ))}
     </div>
   );
 }
--- exercises/module2/b11/11.2/fixation2/src/UserProfile.js
+++ exercises/module2/b11/11.2/fixation2/src/UserProfile.js
@@ -8,6 +8,8 @@ class UserProfile extends React.Component {
         <span>{name}, tem o seguinte email: {email}</span>
         <br />
         <span>Além disso, seu id é {id} e seu avatar é esse aqui: <img src={avatar} alt="avatar" width="100"></img></span>
+        <br />
+        <br />
       </div>
     )
   }
