1b2f65ca054e2698ec5f607705abde8dfffac899
lucas <lucasportellaagu@gmail.com>
2021-10-25 12:12:17 -0300

    ex4 part2

--- exercises/module4/python/exercicio4.py
+++ exercises/module4/python/exercicio4.py
@@ -1,7 +1,29 @@
 import json
 
 books = []
+
+def percentage(category):
+    return  (category * 100) / len(books)
+
 with open("books.json") as file:
     for jsonObj in file:
         booksDict = json.loads(jsonObj)
         books.append(booksDict)
+
+categories = {
+    "Python": 0,
+    "Java": 0,
+    "PHP": 0,
+}
+
+for book in books:
+    for category in book["categories"]:
+        if category in categories:
+            categories[category] += 1
+
+
+csv_headers = ['categoria', 'porcentagem']
+csv_data = {}
+
+for category in categories:
+    csv_data[category] = percentage(categories[category])
