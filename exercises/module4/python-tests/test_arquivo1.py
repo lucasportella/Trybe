6acea8be35addfa682e426fe2b0538e9bad58e78
lucas <lucasportellaagu@gmail.com>
2021-10-26 21:23:58 -0300

    python test

--- exercises/module4/python-tests/test_arquivo1.py
+++ exercises/module4/python-tests/test_arquivo1.py
@@ -1,2 +1,5 @@
 from arquivo1 import is_even
-print(is_even(3))
+
+def test_is_even_if_number_is_even_returns_true():
+    'Retorna True se um número é ímpar, senão False.'
+    assert is_even(2) is True
