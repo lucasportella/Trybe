61d0c5a068a139684b8150e3a8ad7aba37f9933b
lucas <lucasportellaagu@gmail.com>
2021-10-26 21:04:43 -0300

    organize folders and start python test folder

--- exercises/module4/list-comprehension/teste1.py
+++ exercises/module4/list-comprehension/teste1.py
@@ -1,17 +1,5 @@
-numeros = [1,2,3,4,5]
-divisao = [numero / 2 for numero in numeros]
-print(divisao)
+names = ['luiz', 'miranda', 'helena', 'joana', 'felipe']
+new_names = [name[::-1].title()[::-1] for name in names]
 
-#pense no map do javascript
-
-# a primeira palavra numero é a variável de retorno, ou seja, a variável divisão vai receber um array com essa variável numero dentro a cada loop do for que ocorrer, assim como ocorre no retorno do map do javascript
-
-# a segunda palavra numero é a variavel que criamos para o laço for(tem q ser igual a primeira palavra)
-
-# e a variável numeros é a lista que estamos percorrendo
-
-# o que realmente está ocorrendo é:
-# for numero in numeros:
-    # divisao.append(numero)
-
-# importante percebe que o primeiro numero é a variável manipulável, então podemos fazer o que quisermos com ela, ex:
+a = 'miranda'
+print(a[::-1].title()[::-1])
---
+++ exercises/module4/python-tests/test_arquivo1.py
@@ -0,0 +1,2 @@
+from arquivo1 import is_even
+print(is_even(3))
