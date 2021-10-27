6dfd946ca4b842847d5817bb75491bf27a5d8715
lucas <lucasportellaagu@gmail.com>
2021-10-27 18:46:31 -0300

    finish python tests lecture

--- exercises/module4/python-tests/arquivo1.py
+++ exercises/module4/python-tests/arquivo1.py
@@ -1,2 +1,5 @@
 def is_even(number):
     return number % 2 == 0
+
+def divide_by_zero(number):
+    return number / 0
--- exercises/module4/python-tests/test_arquivo1.py
+++ exercises/module4/python-tests/test_arquivo1.py
@@ -1,5 +1,14 @@
-from arquivo1 import is_even
+import pytest
+from arquivo1 import is_even, divide_by_zero
 
-def test_is_even_if_number_is_even_returns_true():
+def test_is_even():
     'Retorna True se um número é ímpar, senão False.'
     assert is_even(2) is True
+
+def test_is_even_fail():
+    with pytest.raises(AssertionError, match="assert False is True"):
+        assert is_even(3) is True
+
+def test_divide_by_zero_throws_exception():
+    with pytest.raises(ZeroDivisionError, match="division by zero"):
+        divide_by_zero(2)
