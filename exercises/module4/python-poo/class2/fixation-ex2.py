933069b689983643db1e9af0037b235f841e77a6
lucas <lucasportellaagu@gmail.com>
2021-11-16 21:26:19 -0300

    class2 - fixation exercise 2

---
+++ exercises/module4/python-poo/class2/fixation-ex2.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+import json
+import csv
+
+
+class SalesReport(ABC):
+    def __init__(self, export_file):
+        self.export_file = export_file
+
+    def build(self):
+        return [{
+                'Coluna 1': 'Dado 1',
+                'Coluna 2': 'Dado 2',
+                'Coluna 3': 'Dado 3'
+                },
+                {
+                'Coluna 1': 'Dado A',
+                'Coluna 2': 'Dado B',
+                'Coluna 3': 'Dado C'
+                }]
+
+    @abstractmethod
+    def serialize(self):
+        raise NotImplementedError
+
+
+class SalesReportJSON(SalesReport):
+    def serialize(self):
+        with open(self.export_file + '.json', 'w') as file:
+            json.dump(self.build(), file)
+
+class SalesReportCSV(SalesReport):
+    def serialize(self):
+        with open(self.export_file + '.csv', 'w') as file:
+            headers = ["Coluna 1", "Coluna 2", "Coluna 3"]
+            writer = csv.DictWriter(file, fieldnames=headers)
+            writer.writeheader()
+            for row in self.build():
+                writer.writerow(row)
+
+relatorio_de_vendas = SalesReportJSON('meu_relatorio')
+relatorio_de_vendas.serialize()
+
+relatorio_de_compras = SalesReportCSV('meu_relatorio')
+relatorio_de_compras.serialize()
