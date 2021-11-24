0f0b4c3f25d394e526f6aaf0cdd753074cc1e355
lucas <lucasportellaagu@gmail.com>
2021-11-24 14:28:56 -0300

    finish poo class2

--- exercises/module4/python-poo/class2/test.py
+++ exercises/module4/python-poo/class2/test.py
@@ -4,84 +4,81 @@ import json
 from zipfile import ZipFile
 
 
-class ZipCompressor():
-   ''' Nossos compressores terão fixado o local de salvamento
-   do arquivo, então vamos defini-lo nos construtores'''
-   def __init__(self, filepath='./'):
-       self.filepath = filepath
-
-   def compress(self, file_name):
-       with ZipFile(file_name + '.zip', 'w') as zip_file:
-           zip_file.write(file_name)
+class Serializer(ABC):
+    @abstractmethod
+    def serialize(cls, data):
+        raise NotImplementedError
 
 
-class GzCompressor():
-   def __init__(self, filepath='./'):
-       self.filepath = filepath
+class ZipCompressor(Serializer):
+    FILE_PATH = './'
 
-   def compress(self, file_name):
-       with open(file_name, 'rb') as content:
-           with gzip.open(file_name + '.gz', 'wb') as gzip_file:
-               gzip_file.writelines(content)
+    '''Um método de classe é chamado diretamente da classe,
+    sem uma instância, e ACESSA algum atributo ou método da classe!'''
+    @classmethod
+    def compress(cls, file_name):
+        '''Repare que, acima, o atributo cls é como o self: o
+        cls é a própria classe, passada automaticamente para
+        um método de classe, enquanto o self é a instância'''
+        with ZipFile(cls.FILE_PATH + file_name + '.zip', 'w') as zip_file:
+            zip_file.write(file_name)
+
+
+class GzCompressor(Serializer):
+    '''Um método estático é chamado diretamente da classe,
+    sem uma instância, e NÃO ACESSA nenhum atributo ou método da classe!'''
+    @staticmethod
+    def compress(file_name):
+        '''Como métodos estáticos não acessam classe nem instância,
+        o Python não dá a eles nenhum primeiro parâmetro'''
+        with open(file_name, 'rb') as content:
+            with gzip.open(file_name + '.gz', 'wb') as gzip_file:
+                gzip_file.writelines(content)
 
 
 class SalesReport(ABC):
-   # Nossa classe agora espera *instâncias* de compressor como um parâmetro.
-   # Temos um valor padrão para suportar o código que usava as SalesReport
-   # sem parâmetros -- não precisa correr pra re-escrever nada ;)
-   # o compressor=GzCompressor() significa que vai receber o compressor Gz como default, a não ser que outro compressor seja escolhido na hora da instanciar a classe 
+    FILE_EXTENSION = ''
 
+    def __init__(self, export_file, compressor=GzCompressor):
+        self.export_file = export_file
+        self.compressor = compressor
 
-   def __init__(self, export_file, compressor=GzCompressor()):
-       self.export_file = export_file
-       self.compressor = compressor
+    def build(self):
+        return [{
+                'Coluna 1': 'Dado 1',
+                'Coluna 2': 'Dado 2',
+                'Coluna 3': 'Dado 3'
+                },
+                {
+                'Coluna 1': 'Dado A',
+                'Coluna 2': 'Dado B',
+                'Coluna 3': 'Dado C'
+                }]
 
-   def build(self):
-       return [{
-               'Coluna 1': 'Dado 1',
-               'Coluna 2': 'Dado 2',
-               'Coluna 3': 'Dado 3'
-               },
-               {
-               'Coluna 1': 'Dado A',
-               'Coluna 2': 'Dado B',
-               'Coluna 3': 'Dado C'
-               }]
+    def get_export_file_name(self):
+      return self.export_file + self.FILE_EXTENSION
 
-   # Aqui temos um atributo de classe!
-   FILE_EXTENSION = ''
+    def compress(self):
+        self.serialize()
+        self.compressor.compress(self.get_export_file_name())
 
-   def get_export_file_name(self):
-     # Aqui usamos o atributo de classe
-     # Vai fazer mais sentido nas classes herdeiras!
-     return self.export_file + self.FILE_EXTENSION
-
-   def compress(self):
-       self.serialize()
-       self.compressor.compress(self.get_export_file_name())
-
-   @abstractmethod
-   def serialize(self):
-       raise NotImplementedError
 
 class SalesReportJSON(SalesReport):
-   # Nós não reimplementamos o get_export_file_name
-   # mas como ele usa um atributo de classe pra pegar a extensão
-   # só de redefinir o atributo já vamos conseguir mudar o resultado!
-   FILE_EXTENSION = '.json'
+    FILE_EXTENSION = '.json'
 
-   def serialize(self):
-       with open(self.get_export_file_name(), 'w') as file:
-           json.dump(self.build(), file)
+    def serialize(self):
+        with open(self.export_file + '.json', 'w') as file:
+            json.dump(self.build(), file)
 
 
 class SalesReportCSV(SalesReport):
-   # Sua implementação vai aqui
-   pass
+    # Sua implementação vai aqui
+    pass
+
 
 # Para testar
 relatorio_de_compras = SalesReportJSON('meu_relatorio_1')
-relatorio_de_vendas = SalesReportJSON('meu_relatorio_2', ZipCompressor())
+relatorio_de_vendas = SalesReportJSON('meu_relatorio_2', ZipCompressor)
 
 relatorio_de_compras.compress()
 relatorio_de_vendas.compress()
