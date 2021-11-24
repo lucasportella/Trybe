2f367966fbc7dfcafaf912c41c4de24bfe3c9df1
lucas <lucasportellaagu@gmail.com>
2021-11-24 20:34:51 -0300

    moda method

--- exercises/module4/python-poo/class2/ex2.py
+++ exercises/module4/python-poo/class2/ex2.py
@@ -22,5 +22,26 @@ class Estatistica():
         if len(lista_em_ordem) % 2 != 0:
             meio = int(len(lista_em_ordem) // 2) + 1
             return lista_em_ordem[meio -1]
+    
+    @classmethod
+    def moda(cls, lista):
+        numero_moda = "não_inicializado"
+        sem_repetição = []
+        mais_repetições = 0
+        contador = 0
+        for numero in lista:
+            if numero not in sem_repetição:
+                sem_repetição.append(numero)
+        for numero_unico in sem_repetição:
+            for numero in lista:
+                if numero == numero_unico:
+                    contador += 1
+            print(contador)
+            if contador > mais_repetições:
+                mais_repetições = contador
+                numero_moda = numero_unico
+            contador = 0
+        return {"numero moda":numero_moda, "repetições": numero_unico}
 
-print(Estatistica.mediana([5,2,9,4,1,4,3,7,6]))
+
+print(Estatistica.moda([5,33,2,5,9,4,1,33,1,4,3,7,6,33,33,33]))
