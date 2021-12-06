586a71f2d23c99a81a3a8cead395c7f2ee00327a
lucas <lucasportellaagu@gmail.com>
2021-12-06 12:26:49 -0300

    full scrap

--- exercises/module4/raspagem-de-dados/crawler/scrap_description.py
+++ exercises/module4/raspagem-de-dados/crawler/scrap_description.py
@@ -5,28 +5,22 @@ import requests
 URL_BASE = "http://books.toscrape.com/catalogue/"
 # Define a primeira página como próxima a ter seu conteúdo recuperado
 next_page_url = 'page-1.html'
+
 while next_page_url:
-    # Busca o conteúdo da próxima página
-    response = requests.get(URL_BASE + next_page_url)
-    selector = Selector(text=response.text)
-    # Imprime os produtos de uma determinada página
-    for product in selector.css(".product_pod"):
-        # Busca e extrai o título e  o preço
-        # title = product.css("h3 a::attr(title)").get()
-        # price = product.css(".price_color::text").get()
-        # print(title, price)
+    page = requests.get(URL_BASE + next_page_url)
+    page_selector = Selector(text=page.text)
+    page_books = page_selector.css(".product_pod")
+    for book in page_books:
+        print(book.css("h3 a::attr(title)").get())
+        print(book.css(".product_price .price_color::text").get())
+        book_url = book.css("h3 a::attr(href)").get()
+        book_page = requests.get(URL_BASE + book_url)
+        book_selector = Selector(text=book_page.text)
+        book_description = book_selector.css("#product_description ~ p::text").get()
+        print(book_description.split()[0])
+        # reduzi a descrição a uma palavra só para não ficar tão grande o ouput
+    print('///////////////////////////////////')
+    print('PAGE END')
+    print('///////////////////////////////////')
 
-        # Busca o detalhe de um produto
-        detail_href = product.css("h3 a::attr(href)").get()
-        detail_page_url = URL_BASE + detail_href
-
-        # Baixa o conteúdo da página de detalhes
-        detail_response = requests.get(detail_page_url)
-        detail_selector = Selector(text=detail_response.text)
-
-        # Extrai a descrição do produto
-        description = detail_selector.css("#product_description ~ p::text").get()
-        print(description)
-
-    # Descobre qual é a próxima página
-    next_page_url = selector.css(".next a::attr(href)").get()
+    next_page_url = page_selector.css(".next a::attr(href)").get()
