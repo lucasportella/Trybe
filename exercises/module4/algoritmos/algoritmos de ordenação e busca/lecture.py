d2658f833ee7feb8073e3362b39e950cbf3536f9
lucas <lucasportellaagu@gmail.com>
2021-12-29 22:47:08 -0300

    insertion_sort

--- exercises/module4/algoritmos/algoritmos de ordenação e busca/lecture.py
+++ exercises/module4/algoritmos/algoritmos de ordenação e busca/lecture.py
@@ -1,30 +1,32 @@
 # # Vamos supor os números não ordenados
-# - ordenados =
-# - não ordenados = 3 6 1 7
+# - coleção = 3 2 1 7
 
-# # Buscamos entre os elementos não ordenados o menor elemento
-# - menor = 1
+# # vamos pegar o primeiro elemento e movê-lo até sua posição
+# - elemento = 3
 
-# # Vamos adicioná-lo a lista de elementos ordenados
-# - ordenados = 1
-# - não ordenados = 6 3 7
+# # como não há elementos a esquerda de 3 não o movemos
+# - coleção = 3 2 1 7
 
-# # Agora repetimos o passo de busca
-# - menor = 3
+# # agora vamos pegar o segundo elemento
+# - elemento = 2
 
-# # Assim teremos
-# - ordenados = 1 3
-# - não ordenados = 6 7
+# # vamos movê-lo à esquerda, enquanto seu valor for menor
+# # que o elemento a sua esquerda
+#              ⤺
+# - coleção = 2 3 1 7
 
-# # Como ainda não esgotamos os números a serem ordenados repetiremos a busca
-# menor = 6
+# # próximo elemento da coleção
+# - elemento = 1
 
-# # Agora temos quase todos os elementos ordenados
-# - ordenados = 1 3 6
-# - não ordenados = 7
+# # vamos inseri-lo na  posição correta,
+# # movendo para a esquerda enquanto seu valor for menor
+# # que o elemento a esquerda
+#              ⤺ ⤺
+# - coleção = 1 2 3 7
 
-# # Faremos a busca pelo menor elemento novamente (único)
-# - menor = 7
+# # por fim verificamos o último elemento
+# - elemento = 7
 
-# # Esgotamos as possibilidades e temos nossa lista ordenada
-# - ordenados = 1 3 6 7
+# # não há elementos menores a esquerda
+# # e a coleção está ordenada
+# - coleção = 1 2 3 7
