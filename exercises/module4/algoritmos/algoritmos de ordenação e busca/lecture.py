1229f9c72e1035a7d7f52c83b7706fed4d5ab531
lucas <lucasportellaagu@gmail.com>
2021-12-30 13:46:38 -0300

    merge, bubble and quick sort

--- exercises/module4/algoritmos/algoritmos de ordenação e busca/lecture.py
+++ exercises/module4/algoritmos/algoritmos de ordenação e busca/lecture.py
@@ -1,32 +1,48 @@
 # # Vamos supor os números não ordenados
-# - coleção = 3 2 1 7
+# - coleção = 7 1 2 5 4 6 3
 
-# # vamos pegar o primeiro elemento e movê-lo até sua posição
-# - elemento = 3
+# # Faremos a escolha do elemento pivô.
+# # Este elemento será o responsável por particionar (dividir) a lista.
+# # Posso escolher qualquer elemento neste passo e, por isso,
+# # vou escolher o elemento do meio
+# - pivot = 5
 
-# # como não há elementos a esquerda de 3 não o movemos
-# - coleção = 3 2 1 7
+# # Movemos todos os elementos menores para a esquerda e os maiores para a direita (a)
+# - 1 2 4 3    5    7 6
 
-# # agora vamos pegar o segundo elemento
-# - elemento = 2
+# # Precisamos ordenar as coleções geradas,
+# # começando com a lista de elementos inferiores
+# - 1 2 4 3
 
-# # vamos movê-lo à esquerda, enquanto seu valor for menor
-# # que o elemento a sua esquerda
-#              ⤺
-# - coleção = 2 3 1 7
+# # Novamente escolhemos o pivot
+# - pivot = 2
 
-# # próximo elemento da coleção
-# - elemento = 1
+# # Novamente fazemos a divisão (b)
+# - 1    2    4 3
 
-# # vamos inseri-lo na  posição correta,
-# # movendo para a esquerda enquanto seu valor for menor
-# # que o elemento a esquerda
-#              ⤺ ⤺
-# - coleção = 1 2 3 7
+# # A lista à esquerda já não tem como ser particionada,
+# # porém a da direita ainda pode ser particionada (c)
+# - 4 3
 
-# # por fim verificamos o último elemento
-# - elemento = 7
+# # Escolhendo o pivô e movendo os outros elementos
+# # chegaremos a
+# - 3    4
 
-# # não há elementos menores a esquerda
-# # e a coleção está ordenada
-# - coleção = 1 2 3 7
+# # Agora precisamos o processo de conquista,
+# # voltando recursivamente ao passo (c), que pediu para ordenar
+# # a sub coleção 4 3, temos como resposta
+# - 3 4
+# # essa resposta, nos faz retornar ao passo (b), onde concatenaremos ao pivô a ordenação do lado direito e esquerdo,
+# # resultando em
+# - 1 2 3 4
+
+# # Vamos agora ver o outro lado do passo (a)
+# - 7 6
+
+# # realizando o mesmo processo
+# # após escolher o pivô e realizar os movimentos teremos
+# - 6    7
+
+# # Por fim a resposta é utilizada lá no passo (a) para concatenarmos os dois lados
+# # e termos por fim a lista ordenada
+# - 1 2 3 4 5 6 7
