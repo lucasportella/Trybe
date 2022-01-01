459684b980f20442fe5239653995bdd7cbe04c87
lucas <lucasportellaagu@gmail.com>
2022-01-01 17:53:17 -0300

    binary_search done

--- exercises/module4/algoritmos/algoritmos de busca e ordenação/exercícios/ex5.py
+++ exercises/module4/algoritmos/algoritmos de busca e ordenação/exercícios/ex5.py
@@ -1,20 +1,19 @@
 from time import sleep
 
 def binary_search(array, value):
-    high_index = len(array) -1
-    low_index = 0
-    index = (high_index + low_index) // 2
-    while high_index > low_index:
-        sleep(1)
-        print(index)
+    index = len(array) // 2
+    max_length = len(array) - 1
+    min_length = 0
+    while max_length >= min_length:
         if array[index] == value:
-            return f'Valor {value} encontrado na posição {index}.'
+            return f'Valor {value} encontrado na posição {index}'
         elif array[index] < value:
-            # low_index = index + 1
-            index = (low_index + high_index) // 2
+            min_length = index +1
+            index = (max_length + min_length) // 2
         elif array[index] > value:
-            high_index = index - 1
-            index = (high_index + low_index) // 2
+            max_length = index -1
+            index = (max_length + min_length) // 2
     return 'Valor não encontrado.'
 
-print(binary_search([3,5,8,9,17,18,31,42,57,75], 9))
+print(binary_search([3,5,8,9,17,18,31,42,57,75], 1))
+print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], 10))
