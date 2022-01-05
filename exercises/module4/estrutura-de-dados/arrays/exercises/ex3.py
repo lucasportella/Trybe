7d63558799e5e60511f10b8fd840e97d79b7c78d
lucas <lucasportellaagu@gmail.com>
2022-01-05 14:54:53 -0300

    ex4

--- exercises/module4/estrutura-de-dados/arrays/exercises/ex3.py
+++ exercises/module4/estrutura-de-dados/arrays/exercises/ex3.py
@@ -1,9 +1,10 @@
 def good_combos(array):
     counter = 0
     for c in range(0, len(array)):
-        for j in range(c + 1, len(array) - c):
+        for j in range(c + 1, len(array)):
             if array[c] == array[j]:
                 counter += 1
     return counter
 
+print(good_combos([1, 1, 2, 3]))
 print(good_combos([1, 3, 1, 1, 2, 3]))
