679c953a194d9c34d0225f83410abc2932c02f85
lucas <lucasportellaagu@gmail.com>
2022-01-14 11:26:49 -0300

    finish lecture

--- exercises/module4/estrutura-de-dados/node/no_comment_linked_list.py
+++ exercises/module4/estrutura-de-dados/node/no_comment_linked_list.py
@@ -2,6 +2,7 @@ from node import Node
 
 
 class LinkedList:
+
     def __init__(self):
         self.head_value = None
         self.__length = 0
@@ -17,51 +18,28 @@ class LinkedList:
         first_value.next = self.head_value
         self.head_value = first_value
         self.__length += 1
-    
+
     def insert_last(self, value):
         last_value = Node(value)
         current_value = self.head_value
 
-        # Mais abaixo criaremos o método is_empty()
-        # que substituirá a condição deste if
-        if current_value is None:
+        if self.is_empty():
             return self.insert_first(value)
 
         while current_value.next:
             current_value = current_value.next
         current_value.next = last_value
         self.__length += 1
-    
+
     def insert_at(self, value, position):
         if position < 1:
             return self.insert_first(value)
-
-
-        # nossa classe possui o atributo len,
-        # igual ao nativo do python, logo podemos
-        # ver que len 0 são 0 elementos, e ao adicionar
-        # o primeiro elemento, teremos o len de 1
         if position >= len(self):
             return self.insert_last(value)
-
-        # vamos varrer a lista começando com a head
         current_value = self.head_value
-        
-        # da posição 0 até a posição que queremos
-        # adicionar, sendo que a posição começa
-        # pelo fim até chegar em 1, oq dá no mesmo
         while position > 1:
             current_value = current_value.next
             position -= 1
-        
-        # depois que encontra a posição, é criado o
-        # o node com o nome next_value, o next_value
-        # já fazemos apontar para seu devido próx
-        # valor, que é o antigo valor do
-        # current_value.next, já que este ficará 1
-        # posição atrás do next_value, logo
-        # o current_value.next deve receber o next_
-        # value 
         next_value = Node(value)
         next_value.next = current_value.next
         current_value.next = next_value
@@ -74,7 +52,7 @@ class LinkedList:
             value_to_be_removed.next = None
             self.__length -= 1
         return value_to_be_removed
-    
+
     def remove_last(self):
         if len(self) <= 1:
             return self.remove_first()
@@ -88,26 +66,24 @@ class LinkedList:
         previous_to_be_removed.next = None
         self.__length -= 1
         return value_to_be_removed
-    
+
     def remove_at(self, position):
         if position < 1:
             return self.remove_first()
-
-        # se colocar >= len(self) -1 tbm funciona, sendo
-        # mais performático acredito, no caso de remover
-        # o último elemento não vai precisar varrer a lista
-        # inteira
         if position >= len(self):
             return self.remove_last()
 
         previous_to_be_removed = self.head_value
+
         while position > 1:
             previous_to_be_removed = previous_to_be_removed.next
             position -= 1
+
         value_to_be_removed = previous_to_be_removed.next
         previous_to_be_removed.next = value_to_be_removed.next
         value_to_be_removed.next = None
         self.__length -= 1
+
         return value_to_be_removed
 
     def get_element_at(self, position):
@@ -120,15 +96,39 @@ class LinkedList:
             if value_to_be_returned:
                 value_returned = Node(value_to_be_returned.value)
         return value_returned
-    
+
     def is_empty(self):
         return not self.__length
 
-list1 = LinkedList()
-list1.insert_first(5)
-list1.insert_last(7)
-list1.insert_last(4)
-list1.insert_last(8)
-list1.insert_last(9)
-print(list1.get_element_at(2))
-print(list1)
+
+# Para testar, apenas rode o arquivo com: `python3 linked_list_content.py` :)
+if __name__ == "__main__":
+    linked_list = LinkedList()
+
+    print(linked_list.is_empty()) # saída: True
+    linked_list.insert_first(1)
+    print(linked_list) # saída: LinkedList(len=1 value=Node(value=1 next=None))
+
+    linked_list.insert_first(2)
+    print(linked_list) # saída: LinkedList(len=2 value=Node(value=2 next=Node(value=1 next=None)))
+
+    linked_list.insert_last(3)
+    print(linked_list) # saída: LinkedList(len=3 value=Node(value=2 next=Node(value=1 next=Node(value=3 next=None))))
+
+    linked_list.remove_last()
+    print(linked_list) # saída: LinkedList(len=2 value=Node(value=2 next=Node(value=1 next=None)))
+
+    linked_list.remove_first()
+    print(linked_list) # saída: LinkedList(len=1 value=Node(value=1 next=None))
+
+    linked_list.insert_at(5, 1)
+    print(linked_list) # saída: LinkedList(len=2 value=Node(value=1 next=Node(value=5 next=None)))
+
+    linked_list.remove_at(0)
+    print(linked_list) # saída: LinkedList(len=1 value=Node(value=5 next=None))
+
+    linked_list.insert_at(6, 1)
+    linked_list.insert_at(7, 2)
+    linked_list.insert_at(8, 3)
+    linked_list.insert_at(9, 4)
+    print(linked_list.get_element_at(3)) # saída: Node(value=8 next=None)
